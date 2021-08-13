e2fd1f7e09c67021e05488d9515983618fb1ae26
liugddx <liugddx@gmail.com>
2021-08-13 15:10:43 +0800

    feat: flink

--- src/main/java/com/sample/flink/JavaFlinkStreamingDemo.java
+++ src/main/java/com/sample/flink/JavaFlinkStreamingDemo.java
@@ -5,6 +5,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
 
 /**
  * <p>@ClassName JavaFlinkStreamingDemo</p>
@@ -25,13 +26,15 @@ public class JavaFlinkStreamingDemo {
          *  第一个泛形是输入类型
          *  第二个是返回的类型
          */
-        SingleOutputStreamOperator<WordCount> flatStream = stream.flatMap(
-                (FlatMapFunction<String, WordCount>) (value, out) -> {
-                    String[] split = value.split(",");
-                    for (String word : split) {
-                        out.collect(new WordCount(word, 1));
-                    }
-                });
+        SingleOutputStreamOperator<WordCount> flatStream = stream.flatMap(new FlatMapFunction<String, WordCount>() {
+            @Override
+            public void flatMap(String value, Collector<WordCount> out) throws Exception {
+                String[] split = value.split(",");
+                for (String word : split) {
+                    out.collect(new WordCount(word, 1));
+                }
+            }
+        });
         //这里根据对象的字段进行keyBy然后统计，最后聚合
         SingleOutputStreamOperator<WordCount> sum = flatStream.keyBy("word")
                 .timeWindow(Time.seconds(2), Time.seconds(1))
@@ -47,6 +50,10 @@ public class JavaFlinkStreamingDemo {
         public String word;
 
         public long count;
+
+        public WordCount() {
+
+        }
 
         public WordCount(String word, long count) {
             this.word = word;
